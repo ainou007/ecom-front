6603419acf0e4e65a985d3de278ac773fa9f349b
Abdelmounim AINOU <ainou.abdelmounim@gmail.com>
2024-05-05 00:49:22 +0100

    add redux persist to the cart items

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/app.css';
 import AppRouter from '@/routes/appRouter';
 import { Provider } from 'react-redux';
-import store from '@/store';
+import {store, persistor} from '@/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+    <PersistGate persistor={persistor}>
     <Provider store={store}>
       <AppRouter />
     </Provider>
-  </React.StrictMode>
+    </PersistGate>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,43 @@
 import cartSlice from '@/store/cart/cartSlice';
 import categorySlice from '@/store/category/categorySlice';
 import productSlice from '@/store/product/productSlice';
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  PAUSE,
+  REGISTER,
+  PERSIST,
+  REHYDRATE,
+  PURGE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = configureStore({
-  reducer: {
-    categorySlice,
-    productSlice,
-    cartSlice,
-  },
+const persistConf = {
+  key: 'cartItems',
+  storage,
+  whitelist: ['items'],
+};
+
+const reducer = combineReducers({
+  categorySlice,
+  productSlice,
+  cartSlice: persistReducer(persistConf, cartSlice),
 });
 
-export default store;
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, PAUSE, REGISTER, PERSIST, REHYDRATE, PURGE],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+export { store, persistor };
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
